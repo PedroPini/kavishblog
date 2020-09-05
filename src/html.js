c8850621db37c18eb721cbc1c1f76965177321bc
PedroPini <augustopini@gmail.com>
2020-09-05 11:57:30 +0800

    Commit initial project

--- src/html.js
+++ src/html.js
@@ -64,25 +64,6 @@ export default function HTML(props) {
          `,
          }}
        />
-       <script src="https://code.jquery.com/jquery-1.11.2.js"></script>
-
-        <!-- Make sure to include Nelify's authentication library -->
-        <!-- Also available via npm as netlify-auth-providers -->
-        <script src="https://unpkg.com/netlify-auth-providers"></script>
-       <script>
-        $(function() {
-          $("#login").on("click", function(e) {
-            e.preventDefault();
-            var authenticator = new netlify.default ({});
-            authenticator.authenticate({provider:"github", scope: "user"}, function(err, data) {
-              if (err) {
-                return $("#output").text("Error Authenticating with GitHub: " + err);
-              }
-              $("#output").text("Authenticated with GitHub. Access Token: " + data.token);
-            });
-          });
-        });
-      </script>
         {props.postBodyComponents}
         <noscript key="noscript" id="gatsby-noscript">
           This app works best with JavaScript enabled.
