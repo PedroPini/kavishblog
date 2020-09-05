a38cd3e5013ac03030eed04f2aa28b77281ab0fc
PedroPini <augustopini@gmail.com>
2020-09-05 11:00:05 +0800

    Commit initial project

---
+++ src/components/Layout/index.js
@@ -0,0 +1,28 @@
+
+import React from "react"
+import PropTypes from "prop-types"
+import Sidebar from "../Sidebar"
+import * as S from "./styled"
+import GlobalStyles from "../../styles/global"
+import MenuBar from "../MenuBar"
+import { TransitionPortal } from "gatsby-plugin-transition-link"
+const Layout = ({ children }) => {
+  return (
+    <S.LayoutWrapper>
+    <GlobalStyles />
+    <TransitionPortal level="top">
+      <Sidebar />
+    </TransitionPortal>
+    <S.LayoutMain>{children}</S.LayoutMain>
+    <TransitionPortal level="top">
+      <MenuBar />
+    </TransitionPortal>
+    </S.LayoutWrapper>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
